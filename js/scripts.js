afef2618d10c26e58ce24d4ebc43abba52d70eec
Travis Crampton <crampton.travis@gmail.com>
2015-09-18 15:02:42 -0700

    eliminated selector Bug

--- js/scripts.js
+++ js/scripts.js
@@ -19,14 +19,14 @@ meatArray = [];
 $(document).ready(function(){
   $("#add-nonMeat").click(function() {
     $("#new-nonMeats").append(
-      '<div class="new-nonMeat">' + '<label for="nonMeat">Non-Meat Topping</label>' + '<select id="nonMeat" class="form-control nonMeat">' + '<option value="None">None</option>' + '<option value="Basil">Basil</option>' + '<option value="Bell Pepper">Bell Pepper</option>' + '<option value="Extra Cheese">Extra Cheese</option>' + '<option value="Garlic">Garlic</option>' + '<option value="Jalapeno">Jalapeno</option>' + '<option value="Musrhoom">Musrhoom</option>' + '<option value="Olive">Olive</option>' + '<option value="Onion">Onion</option>' + '<option value="Pineapple">Pineapple</option>' + '<option value="Fresh Tomato">Fresh Tomato</option>' + '<option value="Sundried Tomato">Sundried Tomato</option>' + '</select>' +
+      '<div class="new-nonMeat">' + '<label for="nonMeat">Non-Meat Topping</label>' + '<select id="nonMeat" class="form-control nonMeat">' + '<option disabled selected>Pick A Non-Meat</option>' + '<option value="Basil">Basil</option>' + '<option value="Bell Pepper">Bell Pepper</option>' + '<option value="Extra Cheese">Extra Cheese</option>' + '<option value="Garlic">Garlic</option>' + '<option value="Jalapeno">Jalapeno</option>' + '<option value="Musrhoom">Musrhoom</option>' + '<option value="Olive">Olive</option>' + '<option value="Onion">Onion</option>' + '<option value="Pineapple">Pineapple</option>' + '<option value="Fresh Tomato">Fresh Tomato</option>' + '<option value="Sundried Tomato">Sundried Tomato</option>' + '</select>' +
       '</div>' + '</div>'
     );
   });
 
   $("#add-meat").click(function() {
     $("#new-meats").append(
-      '<div class="new-meat">' + '<label for="meat">Meat Topping</label>' + '<select id="meat" class="form-control meat">' + '<option value="None">None</option>' + '<option value="Anchovy">Anchovy</option>' + '<option value="Bacon">Bacon</option>' + '<option value="Canadian Bacon">Canadian Bacon</option>' + '<option value="Chicken">Chicken</option>' + '<option value="Ham">Ham</option>' + '<option value="Linguiça">Linguiça</option>' + '<option value="Meatball">Meatball</option>' + '<option value="Pepperoni">Pepperoni</option>' + '<option value="Salami">Salami</option>' + '<option value="Sausage">Sausage</option>' + '</select>' +
+      '<div class="new-meat">' + '<label for="meat">Meat Topping</label>' + '<select id="meat" class="form-control meat">' + '<option disabled selected>Pick A Meat</option>' + '<option value="Anchovy">Anchovy</option>' + '<option value="Bacon">Bacon</option>' + '<option value="Canadian Bacon">Canadian Bacon</option>' + '<option value="Chicken">Chicken</option>' + '<option value="Ham">Ham</option>' + '<option value="Linguiça">Linguiça</option>' + '<option value="Meatball">Meatball</option>' + '<option value="Pepperoni">Pepperoni</option>' + '<option value="Salami">Salami</option>' + '<option value="Sausage">Sausage</option>' + '</select>' +
       '</div>' + '</div>'
     );
   });
@@ -42,16 +42,24 @@ $(document).ready(function(){
 
       $(".new-nonMeat").each(function() {
         var selectedNonMeat = $(this).find("select.nonMeat option:selected").val();
+        if(selectedNonMeat === "Pick A Non-Meat") {
+          ""
+        } else {
         var newVeg = new nonMeat(selectedNonMeat);
         newPizza.toppings.push(newVeg);
         nonMeatArray.push(selectedNonMeat);
+        }
       });
 
       $(".new-meat").each(function(){
         var selectedMeat = $(this).find("select.meat option:selected").val();
+        if (selectedMeat === "Pick A Meat"){
+          ""
+        } else {
         var newBeef = new Meat(selectedMeat);
         newPizza.toppings.push(newBeef);
-        meatArray.push(selectedMeat);
+        meatArray.push(selectedMeat)
+        }
       });
 
       var pizzaPrice = 8;
@@ -62,18 +70,28 @@ $(document).ready(function(){
         pizzaPrice += 6;
       } else {
         pizzaPrice;
-      }
+      } debugger;
 
       //pizza with  meat price
 
-      if ((newPizza.pizzaSize === "Small") && (meatArray.indexOf("None") === (-1))) {
-        pizzaPrice += meatArray.length;
-        debugger;
-      } else {
-        pizzaPrice += (meatArray.length - 1);
-      }
-
-      alert(pizzaPrice);
+      // if ((newPizza.pizzaSize === "Small") && (meatArray.indexOf("None") === (-1))) {
+      //   pizzaPrice += meatArray.length;
+      // }
+      //
+      // if ((newPizza.pizzaSize === "Medium") && (meatArray.indexOf("None") === (-1))) {
+      //   pizzaPrice += (meatArray.length*1.40)
+      // }
+      //
+      // if ((newPizza.pizzaSize === "Large") && (meatArray.indexOf("None") === (-1))) {
+      //   pizzaPrice += (meatArray.length*2)
+      // } else {
+      //   pizzaPrice += (meatArray.length -(1))
+      // } else
+      //
+      // else {
+      //   pizzaPrice += (meatArray.length - 1);
+      // }
+      //
 
 
 
