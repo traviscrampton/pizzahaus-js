a96f400cb53af4bfc45cdf66457f998e00a8cb05
Travis Crampton <crampton.travis@gmail.com>
2015-09-18 11:59:31 -0700

    takes out values for the meat and non meat options

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,15 @@ function Topping(meat, nonMeat){
 }
 
 $(document).ready(function(){
-  $("#add-meat").click(function() {
-    $("#new-meats").append(
+  $("#add-nonMeat").click(function() {
+    $("#new-nonMeats").append(
       '<div class="new-nonMeat">' + '<label for="nonMeat">Non-Meat Topping</label>' + '<select id="nonMeat" class="form-control">' + '<option value="None">None</option>' + '<option value="Basil">Basil</option>' + '<option value="Bell Pepper">Bell Pepper</option>' + '<option value="Extra Cheese">Extra Cheese</option>' + '<option value="Garlic">Garlic</option>' + '<option value="Jalapeno">Jalapeno</option>' + '<option value="Musrhoom">Musrhoom</option>' + '<option value="Olive">Olive</option>' + '<option value="Onion">Onion</option>' + '<option value="Pineapple">Pineapple</option>' + '<option value="Fresh Tomato">Fresh Tomato</option>' + '<option value="Sundried Tomato">Sundried Tomato</option>' + '</select>' +
       '</div>' + '</div>'
     );
   });
 
-  $("#add-nonMeat").click(function() {
-    $("#new-nonMeats").append(
+  $("#add-meat").click(function() {
+    $("#new-meats").append(
       '<div class="new-meat">' + '<label for="meat">Meat Topping</label>' + '<select id="meat" class="form-control">' + '<option value="None">None</option>' + '<option value="Anchovy">Anchovy</option>' + '<option value="Bacon">Bacon</option>' + '<option value="Canadian Bacon">Canadian Bacon</option>' + '<option value="Chicken">Chicken</option>' + '<option value="Ham">Ham</option>' + '<option value="Linguiça">Linguiça</option>' + '<option value="Meatball">Meatball</option>' + '<option value="Pepperoni">Pepperoni</option>' + '<option value="Salami">Salami</option>' + '<option value="Sausage">Sausage</option>' + '</select>' +
       '</div>' + '</div>'
     );
@@ -31,5 +31,14 @@ $(document).ready(function(){
 
     var selectedSize = $("select#pizzaSize option:selected").val();
     var selectedQuantity = parseInt($("select#quantity option:selected").val());
+
+    var newPizza = new Pizza(selectedSize, selectedQuantity);
+    $(".new-topping").each(function() {
+      var selectedNonMeat = $(this).find("select.nonMeat option:selected").val();
+      var selectedMeat = $(this).find("select.meat option:selected").val();
+      var newTopping = new Topping(selectedMeat, selectedNonMeat);
+      newPizza.toppings.push(newTopping);
+debugger;
+    });
   });
 });
